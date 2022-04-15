ab98e238f9c99742dc0702bb356b15b894393039
earthbardock <abhi@earthtechnologies.io>
2022-04-15 13:27:43 +0530

    did file in Candid UI browser

--- rust/hello/src/hello/lib.rs
+++ rust/hello/src/hello/lib.rs
@@ -1,4 +1,16 @@
+use ic_kit::candid::candid_method;
+
 #[ic_cdk_macros::query]
+#[candid_method(query)]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+
+// ---------------- CANDID -----------------------
+
+#[ic_cdk_macros::query(name = "__get_candid_interface_tmp_hack")]
+#[candid_method(query, rename = "__get_candid_interface_tmp_hack")]
+fn export_candid() -> String {
+    include_str!("hello.did").to_string()
 }
